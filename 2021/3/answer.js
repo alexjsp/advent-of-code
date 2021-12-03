051c6696566117daee7ce3e24ab642c2a6a72a6b
Alex Stevenson-Price <alex@psmail.co>
2021-12-03 18:11:12 +0000

    Day 3, part 2 and refactor of part 1 with knowledge from part 2

--- 2021/3/answer.js
+++ 2021/3/answer.js
@@ -14,14 +14,20 @@ for (var i = 0; i < numbers.length; i++) {
 	}
 }
 
-console.log(totalOnesPerPosition);
+function mostCommonBitInPosition(numbers, position) {
+	var values = numbers.map(x => x[position]);
+	return values.filter(x => x == "1").length >= numbers.length / 2 ? "1" : "0";
+}
+
+function leastCommonBitInPosition(numbers, position) {
+	return mostCommonBitInPosition(numbers, position) == "1" ? "0" : "1";
+}
 
 var gammaRate = "";
 var epsilonRate = "";
-for (var i = 0; i < totalOnesPerPosition.length; i++) {
-	var oneMoreCommon = totalOnesPerPosition[i] >= numbers.length/2;
-	gammaRate += oneMoreCommon ? "1" : "0";
-	epsilonRate += oneMoreCommon ? "0" : "1";
+for (var i = 0; i < numbers[0].length; i++) {
+	gammaRate += mostCommonBitInPosition(numbers, i);
+	epsilonRate += leastCommonBitInPosition(numbers, i);
 }
 var gammaRateDecimal = parseInt(gammaRate, 2);
 var epsilonRateDecimal = parseInt(epsilonRate, 2);
@@ -35,3 +41,29 @@ console.log("Answer: " + (gammaRateDecimal * epsilonRateDecimal));
 // Part 2
 console.log("");
 console.log("--------- Part 2 ---------");
+var viableOxygenRatings = numbers;
+for (var i = 0; i < numbers[0].length; i++) {
+	if (viableOxygenRatings.length < 2) {
+		break;
+	}
+
+	var mostCommonBit = mostCommonBitInPosition(viableOxygenRatings, i);
+	viableOxygenRatings = viableOxygenRatings.filter(x => x[i] == mostCommonBit);
+}
+var oxygenGeneratorRating = viableOxygenRatings[0].join("");
+var oxygenGeneratorRatingDecimal = parseInt(oxygenGeneratorRating, 2);
+console.log("Oxygen Generator Rating: " + oxygenGeneratorRating + " (" + oxygenGeneratorRatingDecimal + ")");
+
+var viableCO2Ratings = numbers;
+for (var i = 0; i < numbers[0].length; i++) {
+	if (viableCO2Ratings.length < 2) {
+		break;
+	}
+
+	var leastCommonBit = leastCommonBitInPosition(viableCO2Ratings, i);
+	viableCO2Ratings = viableCO2Ratings.filter(x => x[i] == leastCommonBit);
+}
+var co2Rating = viableCO2Ratings[0].join("");
+var co2RatingDecimal = parseInt(co2Rating, 2);
+console.log("CO2 Scrubber Rating: " + co2Rating + " (" + co2RatingDecimal + ")");
+console.log("Answer: " + (oxygenGeneratorRatingDecimal * co2RatingDecimal));
